95484e8b96fa0f09fb80d5d56b581ac7bf79af80
Pivotal Tracker <labs-tracker-team@pivotal.io>
2015-12-22 08:31:59 -0700

    sizzlin' saucin'

--- app/models/build.rb
+++ app/models/build.rb
@@ -2,6 +2,22 @@ require 'net/http'
 
 class Build < ActiveRecord::Base
   def self.recent(token)
-    JSON.parse(Net::HTTP.get(URI.parse("https://circleci.com/api/v1/recent-builds?circle-token=#{token}")))
+    filter_builds(
+      JSON.parse(
+        Net::HTTP.get(
+          URI.parse("https://circleci.com/api/v1/recent-builds?circle-token=#{token}")
+        )
+      )
+    )
+  end
+
+  private
+
+  def self.filter_builds(builds)
+    builds.reject do |build|
+      build["status"] == 'not_run' ||
+      build["branch"] == 'green-latest' ||
+      build["reponame"] != 'tracker-frontend'
+    end
   end
 end
